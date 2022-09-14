b5463c00e6847db759ea752315f5cb6e6ec63585
ejhem <113192777+ejhem@users.noreply.github.com>
2022-09-14 13:06:18 -0300

    added some JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
+/*
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
-/*
+
 if (topic === "HTML")  {
     console.log("Let's study HTML!");
 } else if (topic === "CSS") {
@@ -16,3 +16,9 @@ if (topic === "HTML")  {
     console.log('Please try again!');
 }
 */
+
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
